42376ccf039a37d7596c2d3ccda3cc16f240daa3
bestkub00123 <107583782+bestkub00123@users.noreply.github.com>
2022-06-16 15:35:47 +0700

    Update KBTG.py

--- KBTG.py
+++ KBTG.py
@@ -4,7 +4,9 @@ Batman.org 19b
 123.pdf 20lb
 ab.com 200tb
 pic.jpeg 10gb
-abc.py 10qb"""
+abc.py 10qb
+ok.gif 1mb
+org.map 100gb"""
 
 list_s = (s.split('\n'))
 split_list_s = [i.split() for i in list_s]
@@ -20,9 +22,11 @@ for i in split_list_s:
             collect.append(file[len(file)-1])
 for p in type_s:
     if 'k' in p:
-        type_s[type_s.index(p)] = str(int(p.replace('kb',''))*(10**3))+'b'       
+        type_s[type_s.index(p)] = int(p.replace('kb',''))*(10**3)      
     if 'm' in p:
-        type_s[type_s.index(p)] = str(int(p.replace('mb',''))*(10**6))+'b'
+        type_s[type_s.index(p)] = int(p.replace('mb',''))*(10**6)
+    if 'g' in p:
+        type_s[type_s.index(p)] = int(p.replace('gb',''))*(10**9)
     if 'a' in p or 'c' in p or 'd' in p or 'e' in p or 'f' in p or 'h' in p or 'i' in p or 'j' in p or 'l' in p or 'n' in p or 'o' in p or 'p' in p or 'q' in p or 'r' in p or 's' in p or 't' in p or 'u' in p or 'v' in p or 'w' in p or 'x' in p or 'y' in p or 'z' in p:
         type_s[type_s.index(p)] = 'Type Error'
 for i in reversed(range (len(type_s))):
@@ -30,5 +34,3 @@ for i in reversed(range (len(type_s))):
         type_s.pop(i)
         collect.pop(i)
 print([[collect[q],type_s[q]] for q in range((len(collect)))])
-"""for q in range((len(collect))):
-    output_s.append([collect[q],type_s[q]])"""
